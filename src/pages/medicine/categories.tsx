43a183376c5859756d657bcf1686623cc24f8e01
wenzhilong <wenzhilong@wenzhilong.com>
2024-07-07 15:27:09 +0800

    药品分类 page 添加动作模态框 ui

--- src/pages/medicine/categories.tsx
+++ src/pages/medicine/categories.tsx
@@ -1,57 +1,105 @@
-import {Button, Card, Form, Input, Space, Table} from "antd";
+import {Button, Card, Form, Input, message, Modal, Space, Table} from "antd";
 import {PlusOutlined, SearchOutlined} from '@ant-design/icons'
+import {useState} from "react";
 
 function MedicineCategories() {
+
+    const [isShow, setIsShow] = useState(false) // 控制modal显示和隐藏
+
     return (
-        <Card
-            title='药品分类'
-            extra={
-                <>
-                    <Button type='primary' icon={<PlusOutlined/>}/>
-                </>
-            }
-        >
-            <Space direction='vertical' style={{width: '100%'}}>
-                <Form layout='inline'>
-                    <Form.Item label='名字'>
-                        <Input placeholder='请输入关键词'/>
+        <>
+            <Card
+                title='药品分类'
+                extra={
+                    <>
+                        <Button
+                            type='primary'
+                            icon={<PlusOutlined/>}
+                            onClick={() => {
+                                setIsShow(true)
+                            }}
+                        />
+                    </>
+                }
+            >
+                <Space direction='vertical' style={{width: '100%'}}>
+                    <Form layout='inline'>
+                        <Form.Item label='名字'>
+                            <Input placeholder='请输入关键词'/>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type='primary' icon={<SearchOutlined/>}/>
+                        </Form.Item>
+                    </Form>
+                    <Table
+                        columns={[
+                            {
+                                title: '序号',
+                                width: 80,
+                                align: 'center'
+                            },
+                            {
+                                title: '名字',
+                                width: 80,
+                                align: 'center'
+                            },
+                            {
+                                title: '主图',
+                                width: 120,
+                                align: 'center'
+                            },
+                            {
+                                title: '简介',
+                                align: 'center'
+                            },
+                            {
+                                title: '操作',
+                                width: 100,
+                                align: 'center'
+                            },
+                        ]}
+                    >
+
+                    </Table>
+                </Space>
+            </Card>
+
+
+            <Modal
+                title='编辑'
+                open={isShow}
+                // 点击遮罩层不关闭
+                maskClosable={false}
+                onCancel={() => setIsShow(false)}
+                onOk={() => {
+                    message.success('保存成功')
+                }}
+            >
+                <Form
+                    onClick={(v) => {
+                        console.log(v)
+                    }}
+                    labelCol={{span: 3}}
+                >
+                    <Form.Item
+                        label='名字'
+                        name='name'
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入名字'
+                            }
+                        ]}
+                    >
+                        <Input placeholder='请输入名字'/>
                     </Form.Item>
-                    <Form.Item>
-                        <Button type='primary' icon={<SearchOutlined/>}/>
+                    <Form.Item label='主图'/>
+                    <Form.Item label='简介' name='desc'>
+                        <Input.TextArea placeholder='请输入简介'/>
                     </Form.Item>
                 </Form>
-                <Table
-                    columns={[
-                        {
-                            title: '序号',
-                            width: 80,
-                            align: 'center'
-                        },
-                        {
-                            title: '名字',
-                            width: 80,
-                            align: 'center'
-                        },
-                        {
-                            title: '主图',
-                            width: 120,
-                            align: 'center'
-                        },
-                        {
-                            title: '简介',
-                            align: 'center'
-                        },
-                        {
-                            title: '操作',
-                            width: 100,
-                            align: 'center'
-                        },
-                    ]}
-                >
-
-                </Table>
-            </Space>
-        </Card>
+            </Modal>
+        </>
     )
 }
 
