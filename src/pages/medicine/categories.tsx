36b0ba680486f2b696f1e67893add92276e309fc
wenzhilong <wenzhilong@wenzhilong.com>
2024-07-07 15:11:45 +0800

    药品分类 page 表单 ui

--- src/pages/medicine/categories.tsx
+++ src/pages/medicine/categories.tsx
@@ -1,4 +1,4 @@
-import {Button, Card, Form, Input} from "antd";
+import {Button, Card, Form, Input, Space, Table} from "antd";
 import {PlusOutlined, SearchOutlined} from '@ant-design/icons'
 
 function MedicineCategories() {
@@ -11,14 +11,46 @@ function MedicineCategories() {
                 </>
             }
         >
-            <Form layout='inline'>
-                <Form.Item label='名字'>
-                    <Input placeholder='请输入关键词'/>
-                </Form.Item>
-                <Form.Item>
-                    <Button type='primary' icon={<SearchOutlined/>}/>
-                </Form.Item>
-            </Form>
+            <Space direction='vertical' style={{width: '100%'}}>
+                <Form layout='inline'>
+                    <Form.Item label='名字'>
+                        <Input placeholder='请输入关键词'/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type='primary' icon={<SearchOutlined/>}/>
+                    </Form.Item>
+                </Form>
+                <Table
+                    columns={[
+                        {
+                            title: '序号',
+                            width: 80,
+                            align: 'center'
+                        },
+                        {
+                            title: '名字',
+                            width: 80,
+                            align: 'center'
+                        },
+                        {
+                            title: '主图',
+                            width: 120,
+                            align: 'center'
+                        },
+                        {
+                            title: '简介',
+                            align: 'center'
+                        },
+                        {
+                            title: '操作',
+                            width: 100,
+                            align: 'center'
+                        },
+                    ]}
+                >
+
+                </Table>
+            </Space>
         </Card>
     )
 }
